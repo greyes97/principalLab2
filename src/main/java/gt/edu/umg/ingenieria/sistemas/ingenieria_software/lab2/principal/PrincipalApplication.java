a3d321293c5d4f87bf75220feddc57f1c1f092b1
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:12:47 -0600

    First commit done

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/principal/PrincipalApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/principal/PrincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.principal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
